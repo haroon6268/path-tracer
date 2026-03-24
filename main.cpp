2e26b847af29e5de05435920b5e532b2c44d4ef2
Haroon Almadani <haroon@Haroons-MacBook-Pro.local>
2026-03-24 14:56:06 -0400

    Abstracted Colors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-
+#include "glm/glm.hpp"
 #include <iostream>
+#include <memory>
+
+glm::vec3 calculateColor(glm::uvec2 pos)
+{
+  return {0.2, 0.2, 1};
+}
 
 int main()
 {
@@ -9,23 +15,19 @@ int main()
     int width = 512;
     int height = 512;
     int channels = 3; // RGB
-    unsigned char data[width * height * channels];
+  
+    auto pixels = std::make_unique<std::uint8_t[]>(width * height * channels);
+    
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
-            if((x/64)%2 == 0){
-            int index = (y * width + x) * channels;
-            data[index]     = 0; // Red
-            data[index + 1] = 0;   // Green
-            data[index + 2] = 255;   // Blue
-            }else{
-            int index = (y * width + x) * channels;
-            data[index]     = 0; // Red
-            data[index + 1] = 255;   // Green
-            data[index + 2] = 0;   // Blue
-            }
-        }
+        const auto index = x + y * width;
+        const auto color = calculateColor({x,y});
+        pixels[index * 3 + 0] = color.x * 255;
+        pixels[index * 3 + 1] = color.y * 255;
+        pixels[index * 3 + 2] = color.z * 255;
+      }
     }
 
-    stbi_write_png("output.png", width, height, channels, data, width * channels);
+    stbi_write_png("output.png", width, height, channels, pixels.get(), width * channels);
     return 0;
 }
