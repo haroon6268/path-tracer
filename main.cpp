1efeb4c372dee435a6cb5bd98e450e085ebbb626
Haroon Almadani <haroon@Haroons-MacBook-Pro.local>
2026-03-28 11:07:51 -0400

    Added ray struct

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 #include "glm/glm.hpp"
-#include <iostream>
 #include <memory>
 
-glm::vec3 calculateColor(glm::uvec2 pos)
+glm::vec3 calculateColor(glm::vec2 pixelCoord)
 {
-  auto x = pos.x;
-  auto y = pos.y;
-
-  if((x/16) % 2 == 0 ^ (y/16) % 2 == 0)
-  {
-    return {0,0,0};
-  }
-  else
-  {
-    return {1, 1, 1};
-  }
+  return {pixelCoord * 2.f - 1.f, 1};
 }
+
+struct ray{
+  glm::vec3 origin;
+  glm::vec3 dir;
+  glm::vec3 at(float t)
+  {
+    return origin + (t*dir) ;
+  }
+};
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
     int width = 512;
     int height = 512;
     int channels = 3; // RGB
@@ -31,13 +28,15 @@ int main()
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
         const auto index = x + y * width;
-        const auto color = calculateColor({x,y});
+        glm::vec2 pixelCoord = {x,y};
+        glm::vec2 extent = {width-1, height-1};
+        const auto color = calculateColor({pixelCoord/extent});
         pixels[index * 3 + 0] = color.x * 255;
         pixels[index * 3 + 1] = color.y * 255;
         pixels[index * 3 + 2] = color.z * 255;
       }
     }
-
+    stbi_flip_vertically_on_write(1);
     stbi_write_png("output.png", width, height, channels, pixels.get(), width * channels);
     return 0;
 }
